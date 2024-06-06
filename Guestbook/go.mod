042a2c51dd88a35d37b8c6f525f6f4747a399690
Robert Petz <rpetz@ignitelabs.net>
2024-06-06 10:13:53 -0700

    Straggler file checking

--- Guestbook/go.mod
+++ Guestbook/go.mod
@@ -2,4 +2,7 @@ module github.com/Ignite-Laboratories/JanOS/Guestbook
 
 go 1.22
 
-require github.com/google/uuid v1.6.0
+require (
+	github.com/google/uuid v1.6.0
+	github.com/mattn/go-sqlite3 v2.0.3+incompatible // indirect
+)
--- Guestbook/main.go
+++ Guestbook/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "github.com/Ignite-Laboratories/JanOS/Internal/Component"
+import (
+	"database/sql"
+	_ "github.com/mattn/go-sqlite3"
+	"log"
+)
 
 func main() {
-	Component.Setup()
+	//Component.Setup()
+	db, err := sql.Open("sqlite3", "./guestbook.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+	//db.Exec("create table foo (id integer not null primary key, name text);")
+
+	for {
+	}
 }
