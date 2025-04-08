29d24dd0f80790a901d25636d0cf47d12699867d
Robert Petz <rpetz@ignitelabs.net>
2025-04-08 16:17:21 -0700

    Added an easier way to setup the environment

---
+++ sync_windows.bat
@@ -0,0 +1,8 @@
+git clone https://github.com/ignite-laboratories/core
+git clone https://github.com/ignite-laboratories/fugue
+git clone https://github.com/ignite-laboratories/glitter
+git clone https://github.com/ignite-laboratories/host
+git clone https://github.com/ignite-laboratories/life
+git clone https://github.com/ignite-laboratories/spark
+git clone https://github.com/ignite-laboratories/support
+git clone https://github.com/ignite-laboratories/tiny
