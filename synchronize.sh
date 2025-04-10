32afa873760d9e5db932d87c53bc1a4b44dc8a9b
Robert Petz <rpetz@ignitelabs.net>
2025-04-10 09:13:17 -0700

    Update synchronize.sh

--- synchronize.sh
+++ synchronize.sh
@@ -1,35 +1,40 @@
-echo "Synchronizing JanOS"
-echo "Ignite Laboratories"
+SCRIPT_DIR=$(dirname "$0")
+(
+  cd "$0" || exit
 
-echo
+  echo "Synchronizing JanOS"
+  echo "Ignite Laboratories"
 
-echo "[janos]"
-if [ -d ".git" ]; then
-  git pull
-else
-  git clone "https://github.com/ignite-laboratories/JanOS"
-fi
-
-synchronize() {
   echo
-  if [ -d "$1/.git" ]; then
-    (
-      cd "$1" || exit
-      echo "[$1]"
-      git pull
-    )
-  else
-    echo "[$1]"
-    git clone "https://github.com/ignite-laboratories/$1"
-  fi
-}
 
-# Call the function for multiple repositories
-synchronize core
-synchronize fugue
-synchronize glitter
-synchronize host
-synchronize life
-synchronize spark
-synchronize support
-synchronize tiny
+  echo "[janos]"
+  if [ -d ".git" ]; then
+    git pull
+  else
+    git clone "https://github.com/ignite-laboratories/janos"
+  fi
+
+  synchronize() {
+    echo
+    if [ -d "$1/.git" ]; then
+      (
+        cd "$1" || exit
+        echo "[$1]"
+        git pull
+      )
+    else
+      echo "[$1]"
+      git clone "https://github.com/ignite-laboratories/$1"
+    fi
+  }
+
+  # Call the function for multiple repositories
+  synchronize core
+  synchronize fugue
+  synchronize glitter
+  synchronize host
+  synchronize life
+  synchronize spark
+  synchronize support
+  synchronize tiny
+)
