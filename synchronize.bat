b06eab27e74c5694680ae86eaec8a5e7c9208a31
Robert Petz <rpetz@ignitelabs.net>
2025-04-10 09:20:28 -0700

    Update synchronize.bat

--- synchronize.bat
+++ synchronize.bat
@@ -1,36 +1,48 @@
 @echo off
+setlocal enabledelayedexpansion
+
+:: Get the script directory
+for %%I in ("%~dp0") do set SCRIPT_DIR=%%~fI
+
+:: Change to the script directory
+cd /d "%SCRIPT_DIR%" || exit /b 1
+
 echo Synchronizing JanOS
 echo Ignite Laboratories
+echo.
 
-echo
+:: Function to synchronize a repository
+:sync
+if exist "%SCRIPT_DIR%\%1\.git" (
+    echo [%1]
+    cd /d "%SCRIPT_DIR%\%1" || exit /b 1
+    git pull
+    cd /d "%SCRIPT_DIR%"
+) else (
+    echo [%1]
+    git clone "https://github.com/ignite-laboratories/%1" "%SCRIPT_DIR%\%1"
+)
+exit /b 0
 
-echo [janos]
-if exist .git (
+:: Synchronize JanOS
+if exist ".git" (
+    echo [janos]
     git pull
 ) else (
-    git clone "https://github.com/ignite-laboratories/JanOS"
+    echo [janos]
+    git clone "https://github.com/ignite-laboratories/janos" "%SCRIPT_DIR%\janos"
 )
 
-:: Function-like structure for synchronizing repositories
-:Synchronize
-echo
-if exist "%~1\.git" (
-    echo [%~1]
-    pushd "%~1"
-    git pull
-    popd
-) else (
-    echo [%~1]
-    git clone "https://github.com/ignite-laboratories/%~1"
-)
-goto :eof
+:: Call the sync function for multiple repositories
+call :sync core
+call :sync fugue
+call :sync glitter
+call :sync host
+call :sync life
+call :sync spark
+call :sync support
+call :sync tiny
 
-:: Call the 'Synchronize' subroutine for multiple repositories
-call :Synchronize core
-call :Synchronize fugue
-call :Synchronize glitter
-call :Synchronize host
-call :Synchronize life
-call :Synchronize spark
-call :Synchronize support
-call :Synchronize tiny
+:: End script
+endlocal
+exit /b 0
