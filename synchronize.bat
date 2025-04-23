0016110efba6d0a2ea9c2e9f7d80d8646cb30fad
Robert Petz <rpetz@ignitelabs.net>
2025-04-23 14:54:29 -0700

    Fixed the batch synchronization

--- synchronize.bat
+++ synchronize.bat
@@ -1,50 +1,45 @@
 @echo off
 setlocal enabledelayedexpansion
 
-:: Get the script directory
-for %%I in ("%~dp0") do set SCRIPT_DIR=%%~fI
-
-:: Change to the script directory
-cd /d "%SCRIPT_DIR%" || exit /b 1
+:: Get directory of the script
+for %%I in ("%~f0") do set SCRIPT_DIR=%%~dpI
+cd /d "%SCRIPT_DIR%"
 
 echo Synchronizing JanOS
 echo Ignite Laboratories
 echo.
 
-:: Function to synchronize a repository
-:sync
-if exist "%SCRIPT_DIR%\%1\.git" (
-    echo [%1]
-    cd /d "%SCRIPT_DIR%\%1" || exit /b 1
-    git pull
-    cd /d "%SCRIPT_DIR%"
-) else (
-    echo [%1]
-    git clone "https://github.com/ignite-laboratories/%1" "%SCRIPT_DIR%\%1"
-)
-exit /b 0
-
-:: Synchronize JanOS
-if exist ".git" (
-    echo [janos]
+:: Synchronize the "janos" repository
+echo [janos]
+if exist .git (
     git pull
 ) else (
-    echo [janos]
-    git clone "https://github.com/ignite-laboratories/janos" "%SCRIPT_DIR%\janos"
+    git clone "https://github.com/ignite-laboratories/janos"
 )
 
-:: Call the sync function for multiple repositories
-call :sync arwen
-call :sync core
-call :sync fugue
-call :sync glitter
-call :sync host
-call :sync hydra
-call :sync life
-call :sync spark
-call :sync support
-call :sync tiny
+:: Define the synchronize function
+call :synchronize arwen
+call :synchronize core
+call :synchronize fugue
+call :synchronize glitter
+call :synchronize host
+call :synchronize hydra
+call :synchronize life
+call :synchronize spark
+call :synchronize support
+call :synchronize tiny
 
-:: End script
-endlocal
-exit /b 0
+goto :eof
+
+:synchronize
+echo.
+if exist "%~1\.git" (
+    pushd "%~1"
+    echo [%~1]
+    git pull
+    popd
+) else (
+    echo [%~1]
+    git clone "https://github.com/ignite-laboratories/%~1"
+)
+goto :eof
